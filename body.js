744728c0cda78f436df8a8f4cf27769c46b77b73
“[Sandesh <075bct075.sandesh@pcampus.edu.np>
2021-06-10 18:13:28 +0545

    cp

--- body.js
+++ body.js
@@ -1,15 +1,29 @@
 class Body {
-   constructor(posX, posY) {
+   constructor(posX, posY, tickRate) {
       this.pos = createVector(posX, posY);
       this.vel = createVector(0.1, 0.1);
       this.acc = createVector(0, 0);
+      this.tickRate = tickRate;
+      this.tick = 0;
+
    }
 
-   update() {
-      text(this.vel.x.toFixed(2) + "  " + this.vel.y.toFixed(2), 400, 50);
+   update(body) {
       this.vel.add(this.acc);
       this.pos.add(this.vel);
       this.acc.mult(0);
+      
+      if(this.tick > this.tickRate) {
+         force = p5.Vector.sub(body.pos, this.pos);
+         let d = force.mag();
+         d = constrain(d, 8, 25);
+         strength = G / (d * d);
+         force.setMag(strength);
+         this.applyForce(force);
+         this.tick = 0;
+      }
+      
+      this.tick++;
    }
 
    applyForce(force) {
@@ -20,5 +34,7 @@ class Body {
       stroke(0);
       fill(175);
       ellipse(this.pos.x, this.pos.y, 8, 8);
+      line(this.pos.x, this.pos.y, this.pos.x + this.vel.x * 100, this.pos.y + this.vel.y * 100);
+
    }
 }
--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 let b1, b2;
+let  force, G, strength;
 
 function setup() {
-    createCanvas(800, 600);
-    
-    b1 = new Body(50, 50);
-    b2 = new Body(100, 100);
+   createCanvas(800, 600);
+   G = 20;
+
+   b1 = new Body(100, 100, 20);
+   b2 = new Body(120, 100, 10);
 }
 
 function draw() {
-    background(100);
-    b1.update();
-    b1.display();
-  
+   background(100);
+   text(b1.vel.x.toFixed(2) + "  " + b1.vel.y.toFixed(2) + "  " + b1.tick, 400, 50);
+   b1.update(b2);
+   b2.update(b1);
+   b1.display();
+   b2.display();
 }
-
-
-
-function mousePressed() {
-    wind = new p5.Vector(0.1,0);
-    b1.applyForce(wind);
-}  
